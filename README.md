2ab9b12bb0b53eaae0385317a479144b2115aa18
Vitalii Puhach <vitaliipuhach@Vitaliis-MacBook-Pro.local>
2017-03-18 13:37:19 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+clicker
+=======
+
+A Symfony project created on March 18, 2017, 8:16 am.
---
+++ src/AppBundle/Entity/BadDomain.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Model\Timestampable\Timestampable;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="bad_domains")
+ */
+class BadDomain
+{
+    use Timestampable;
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+}
