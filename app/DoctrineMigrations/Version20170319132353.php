aa3b1052b0d091bac8f095f8bb3d55965ce493ab
VitaliiPuhach <puhach.vitalii@projektcs.com>
2017-03-19 15:36:20 +0200

    added doctrine migration and back to links btn from success and error page

---
+++ app/DoctrineMigrations/Version20170319132353.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170319132353 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE click (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', ua VARCHAR(255) NOT NULL, ip VARCHAR(255) NOT NULL, ref VARCHAR(255) NOT NULL, param1 VARCHAR(255) NOT NULL, param2 VARCHAR(255) NOT NULL, error INT NOT NULL, bad_domain TINYINT(1) NOT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE bad_domains (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, created_at DATETIME DEFAULT NULL, updated_at DATETIME DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE click');
+        $this->addSql('DROP TABLE bad_domains');
+    }
+}
