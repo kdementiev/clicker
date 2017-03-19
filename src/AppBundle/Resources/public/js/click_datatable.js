7eb171179df30988626e4206aafec7bc7c496520
VitaliiPuhach <puhach.vitalii@projektcs.com>
2017-03-19 15:13:03 +0200

    datatable filtering

--- src/AppBundle/Resources/public/js/click_datatable.js
+++ src/AppBundle/Resources/public/js/click_datatable.js
@@ -1,6 +1,6 @@
 ;(function () {
 
-    $('#click-datatable-js').DataTable({
+    var clickDataTable = $('#click-datatable-js').DataTable({
         "lengthChange": false,
         "info": false,
         "iDisplayLength": 5,
@@ -17,9 +17,9 @@
         ],
         "language": {
             // "lengthMenu": "Display _MENU_ records per page",
-            "zeroRecords": "Nothing was found",
+            "zeroRecords": Translator.trans('page.click.show_all.datatable.not_found'),
             "info": "Page _PAGE_ of _PAGES_",
-            "infoEmpty": "No records",
+            "infoEmpty": Translator.trans('page.click.show_all.datatable.no_records'),
             "infoFiltered": "(filtered from _MAX_ records)",
             "search": "Search: ",
             "paginate": {
@@ -28,6 +28,26 @@
                 "next":       "Next",
                 "previous":   "Back"
             }
+        }
+    });
+
+    $('#show-no-errors-js').on('change', function() {
+        var errors = clickDataTable.column(6);
+
+        if (this.checked) {
+            errors.search('0', true, false).draw();
+        } else {
+            errors.search('', true, false).draw();
+        }
+    });
+
+    $('#show-bad-js').on('change', function() {
+        var badDomains = clickDataTable.column(7);
+
+        if (this.checked) {
+            badDomains.search('Yes', true, false).draw();
+        } else {
+            badDomains.search('', true, false).draw();
         }
     });
 
